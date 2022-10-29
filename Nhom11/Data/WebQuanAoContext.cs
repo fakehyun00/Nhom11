544072a8a1e82fdeb7050c43066c3738cb28e305
NgHung111 <thienhungnguyen397@gmail.com>
2022-10-29 15:55:34 +0700

    Tao Data

---
+++ Nhom11/Data/WebQuanAoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nhom11.Models;
+
+namespace Nhom11.Data
+{
+    public class WebQuanAoContext:DbContext
+    {
+        public WebQuanAoContext(DbContextOptions<WebQuanAoContext>options): base(options) { }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceDetail> InvoiceDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+    }
+}
