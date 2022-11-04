5796a8d0d3c03a442f91430ba9e8c40317176ed5
NgHung111 <thienhungnguyen397@gmail.com>
2022-11-04 13:32:16 +0700

    Edit Layout New

---
+++ Nhom11/Controllers/CartsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nhom11.Controllers
+{
+    public class CartsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
